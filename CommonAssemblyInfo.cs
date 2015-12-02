00c8b19672fa5daf915addb2dd043eb736b7906a
Dmitry Shechtman <damnian@gmail.com>
2015-12-02 08:56:25 +0200

    Extract CommonAssemblyInfo Fix AssemblyVersions

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("Andrew Smith")]
+[assembly: AssemblyProduct("SyslogNet.Client")]
+[assembly: AssemblyCopyright("Copyright © Andrew Smith 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: CLSCompliant(true)]
