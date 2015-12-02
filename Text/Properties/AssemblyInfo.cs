148de4f8b28ec158b29146066f99e225d7a7fe69
Dmitry Shechtman <damnian@gmail.com>
2015-12-02 08:54:24 +0200

    Fix Text AssemblyInfo

--- Text/Properties/AssemblyInfo.cs
+++ Text/Properties/AssemblyInfo.cs
@@ -1,29 +1,20 @@
 ﻿using System;
 using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
 [assembly: AssemblyTitle("SyslogNet.Client.Text")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Andrew Smith")]
+[assembly: AssemblyCompany("Xamarin Inc.")]
 [assembly: AssemblyProduct("SyslogNet.Client")]
-[assembly: AssemblyCopyright("Copyright © Andrew Smith 2013")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2014-2015 Xamarin Inc. (www.xamarin.com)")]
+[assembly: AssemblyTrademark("Xamarin Inc.")]
 [assembly: AssemblyCulture("")]
 
 [assembly: CLSCompliant(true)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
