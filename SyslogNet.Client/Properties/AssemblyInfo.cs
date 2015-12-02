5ea166e357c92a3c914831eda19f5792d9dbb74a
Dmitry Shechtman <damnian@gmail.com>
2015-12-03 00:47:42 +0200

    Rename SyslogNet.Client to Transport

--- SyslogNet.Client/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("SyslogNet.Client")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyVersion("0.3.0")]
---
+++ Transport/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("SyslogNet.Client.Transport")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyVersion("0.3.0")]
