d327720370475476e27182309d97dfc67158c923
Dmitry Shechtman <damnian@gmail.com>
2015-12-02 23:34:19 +0200

    Add comments

--- Core/Transport/ISyslogMessageSender.cs
+++ Core/Transport/ISyslogMessageSender.cs
@@ -4,12 +4,38 @@ using SyslogNet.Client.Serialization;
 
 namespace SyslogNet.Client.Transport
 {
+    /// <summary>
+    /// Message sender.
+    /// </summary>
 	public interface ISyslogMessageSender : IDisposable
 	{
+        /// <summary>
+        /// Connects to the remote host.
+        /// </summary>
         void Connect();
+
+        /// <summary>
+        /// Disconnects from the remote host.
+        /// </summary>
         void Disconnect();
+
+        /// <summary>
+        /// Reconnects to the remote host.
+        /// </summary>
 		void Reconnect();
-		void Send(SyslogMessage message, ISyslogMessageSerializer serializer);
-		void Send(IEnumerable<SyslogMessage> messages, ISyslogMessageSerializer serializer);
+
+        /// <summary>
+        /// Sends a message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        /// <param name="serializer">Serializer.</param>
+        void Send(SyslogMessage message, ISyslogMessageSerializer serializer);
+
+        /// <summary>
+        /// Sends a collection of messages.
+        /// </summary>
+        /// <param name="messages">Messages.</param>
+        /// <param name="serializer">Serializer.</param>
+        void Send(IEnumerable<SyslogMessage> messages, ISyslogMessageSerializer serializer);
 	}
 }
