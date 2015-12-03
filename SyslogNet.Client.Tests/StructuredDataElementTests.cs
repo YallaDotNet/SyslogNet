8d0364a56a5991c58e4a751ce85897bbe9ad344e
Dmitry Shechtman <damnian@gmail.com>
2015-12-03 05:25:40 +0200

    Suppress warning

--- SyslogNet.Client.Tests/StructuredDataElementTests.cs
+++ SyslogNet.Client.Tests/StructuredDataElementTests.cs
@@ -14,8 +14,10 @@ namespace SyslogNet.Client.Tests
 			};
 			var sut = new StructuredDataElement("SomeSdId", parameters);
 
+#pragma warning disable 612, 618
 			Assert.Equal("SomeSdId@" + StructuredDataElement.DefaultPrivateEnterpriseNumber, sut.SdId);
-		}
+#pragma warning restore 612, 618
+        }
 
 		[Fact]
 		public void StructuredDataIdWillRetainProvidedPrivateEnterpriseNumber()
